5f291094443beb69bb881ab0018c2cddf274f274
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-02 18:48:49 +0300

    created a Function to generate an 8 character password for a credential

--- user_credentials.py
+++ user_credentials.py
@@ -61,6 +61,12 @@ class Credential:
 		# global users_list
 		Credential.credentials_list.append(self)
 	
+	def generate_password(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+		'''
+		Function to generate an 8 character password for a credential
+		'''
+		gen_pass=''.join(random.choice(char) for _ in range(size))
+		return gen_pass
 	@classmethod
 	def find_by_site_name(cls, site_name):
 		'''
